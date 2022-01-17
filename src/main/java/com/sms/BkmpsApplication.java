ba8bb7492f1bc1d57f6a0dbb44b72a436df54423
Ayush <ayusha25@gmail.com>
2022-01-18 05:29:46 +0530

    BKMPS Project initial setup

---
+++ src/main/java/com/sms/BkmpsApplication.java
@@ -0,0 +1,13 @@
+package com.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkmpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkmpsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sms/action/RegistrationAct.java
@@ -0,0 +1,22 @@
+
+package com.sms.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class RegistrationAct {
+
+@GetMapping("/regHome")
+public ModelAndView studentReg() {
+	ModelAndView mv=new  ModelAndView();
+	mv.setViewName("regSms");
+	return mv;
+}
+
+
+}
